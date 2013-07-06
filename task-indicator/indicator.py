d508675447bbbb0710f3b932a41739fa64876ae7
Justin Forest <hex@umonkey.net>
2013-07-06 10:35:19 +0400

    Move tasks with the 'pin' tag to the top.

--- task-indicator/indicator.py
+++ task-indicator/indicator.py
@@ -133,8 +133,9 @@ class Checker(object):
     def task_sort(self, task):
         """Returns the data to sort tasks by."""
         # print task["urgency"], task["description"]
+        is_pinned = "pin" in task.get("tags", [])
         is_running = "start" in task
-        return -is_running, -float(task["urgency"])
+        return -is_running, -is_pinned, -float(task["urgency"])
 
     def on_show_all_tasks(self, widget):
         self.search_dialog.show_all()
